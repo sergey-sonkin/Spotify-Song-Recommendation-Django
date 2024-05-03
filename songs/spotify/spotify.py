ee699a8216a739afa0be4c59c382ad9a98186a45
Sergey Sonkin <mastersmail1@gmail.com>
2024-05-02 23:52:53 -0400

    Continued work on filtering duplicate albums

--- songs/spotify/spotify.py
+++ songs/spotify/spotify.py
@@ -1,3 +1,7 @@
+import datetime
+from operator import attrgetter
+from typing import Optional
+
 from songs.models import Album, Artist, Song, SongFeatures
 from songs.spotify.spotify_client import SpotifyClient
 from songs.spotify.spotify_client_constants import SpotifyAlbumType
@@ -11,9 +15,9 @@ def get_or_create_artist(artist_id: str) -> tuple[Artist, bool]:
         return Artist.objects.get(id=artist_id), True
     except Artist.DoesNotExist:
         spotify_artist = client.get_artist(artist_id=artist_id)
-        db_artist = Artist.objects.import_spotify_artist(
+        db_artist = Artist.objects.import_spotify_artist(  # pyright: ignore
             spotify_artist
-        )  # pyright: ignore
+        )
         db_artist.save()
         return db_artist, False
 
@@ -28,31 +32,70 @@ def get_artists_albums(db_artist: Artist):
             continue
 
 
-def filter_duplicate_albums(spotify_albums: list[SpotifyAlbum]) -> list[SpotifyAlbum]:
-    names: dict[str, list[SpotifyAlbum]] = {}
+def get_max_keys(input_dict: dict):
+    max_value = max(input_dict.values())
+    keys = [key for key, value in input_dict.items() if value == max_value]
+    return keys, max_value
 
+
+def group_albums(input_albums: list[SpotifyAlbum]) -> dict[str, list[SpotifyAlbum]]:
+    names: dict[str, list[SpotifyAlbum]] = {}
     ## TODO: Create a better system than just filtering on album name
-    for album in spotify_albums:
+    for album in input_albums:
         names[album.name] = [*names.get(album.name, []), album]
+    return names
+
+
+def filter_on_explicit_values(
+    spotify_albums: list[SpotifyAlbum],
+) -> tuple[Optional[SpotifyAlbum], list[SpotifyAlbum]]:
+    ids = [album.id for album in spotify_albums]
+    tracks_dict = SpotifyClient().get_multiple_albums_tracks(album_ids=ids)
+    explicit_albums = [
+        album
+        for album, tracks in zip(spotify_albums, tracks_dict.values())
+        if tracks[0].is_explicit
+    ]
+    match len(explicit_albums):
+        case 1:
+            return explicit_albums[0], []
+        case 0:
+            return None, spotify_albums  ## if none are explicit, we just continue
+        case _:
+            return None, explicit_albums
+
+
+def filter_duplicate_albums(spotify_albums: list[SpotifyAlbum]) -> list[SpotifyAlbum]:
+    grouped_albums = group_albums(input_albums=spotify_albums)
 
     singleton_albums = []
-    for album_name, duplicate_albums in names.items():
+    for album_name, duplicate_albums in grouped_albums.items():
         if len(duplicate_albums) == 1:
             singleton_albums.append(duplicate_albums[0])
             continue
-        ## First - filter on explicit values
-        ids = [album.id for album in duplicate_albums]
-        tracks_dict = SpotifyClient().get_multiple_albums_tracks(album_ids=ids)
-        explicit_album_names = [
-            album_name
-            for album_name, tracks in tracks_dict.items()
-            if tracks[0].is_explicit
-        ]
-        if len(explicit_album_names) == 1:
-            singleton_albums.append(explicit_album_names[0])
-            continue
 
-    return names
+        ## First - filter on explicit values
+        one_explicit_album, remaining_albums_1 = filter_on_explicit_values(
+            duplicate_albums
+        )
+        if one_explicit_album:
+            singleton_albums.append(one_explicit_album)
+
+        ## Second - filter on more recently released
+        most_recent_album = max(remaining_albums_1, key=attrgetter("release_date"))
+        most_recent_albums = [
+            album
+            for album in remaining_albums_1
+            if album.release_date == most_recent_album.release_date
+        ]
+        if len(most_recent_albums) == 1:
+            singleton_albums.append(most_recent_albums[0])
+            continue
+        remaining_albums_2 = most_recent_albums
+
+        ## Third - filter on number of tracks (more is better)
+
+    return singleton_albums
 
 
 ## Steps for new artist
