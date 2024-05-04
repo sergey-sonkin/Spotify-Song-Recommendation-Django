1bbd32f3c4fa14e31b53445b5eb7ee41953b1118
Sergey Sonkin <mastersmail1@gmail.com>
2024-05-04 13:03:57 -0400

    Added test case for album name parsing logic

---
+++ songs/test_spotify_core.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from mock import patch
+
+from songs.spotify.spotify import *
+
+
+class SpotifyClientTestCase(TestCase):
+    def test_get_all_albums_calls(self):
+        client = SpotifyClient()
+
+        with self.subTest(msg="Filter Eminem albums"):
+            eminem_albums = client.get_all_artist_albums("7dGJo4pcD2V6oG8kP0tJRR")
+            filtered = filter_duplicate_albums(eminem_albums)
+            self.assertEqual(
+                len(eminem_albums),
+                len(filtered) + 3,
+                msg="2024/05/04 - 3 albums should get filtered",
+            )
