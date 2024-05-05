2870f1773ac32f1155e70a44ff6cbf9314894979
Sergey Sonkin <mastersmail1@gmail.com>
2024-05-05 15:10:31 -0400

    Remove ignored files

--- songs/models.py
+++ songs/models.py
@@ -2,7 +2,12 @@ from datetime import datetime, timedelta
 
 from django.db import models
 
-from songs.spotify.spotify_serializer import SpotifyAlbum, SpotifyArtist, SpotifyTrack
+from songs.spotify.spotify_serializer import (
+    SpotifyAlbum,
+    SpotifyArtist,
+    SpotifyTrack,
+    SpotifyTrackFeatures,
+)
 
 SPOTIFY_UUID_LENGTH = 22
 ARBITRARY_LENGTH = 50
@@ -94,9 +99,26 @@ class SongFeaturesManager(models.Manager):
         db_song_features = self.filter(song_id=db_songs)
         return db_song_features
 
+    def import_song_features(self, features: SpotifyTrackFeatures):
+        return self.create(
+            id=features.id,
+            acousticness=features.acousticness,
+            danceability=features.danceability,
+            energy=features.energy,
+            instrumentalness=features.instrumentalness,
+            key=features.key,
+            liveness=features.liveness,
+            loudness=features.loudness,
+            mode=1 if features.is_major else 0,
+            speechiness=features.speechiness,
+            tempo=features.tempo,
+            time_signature=features.time_signature,
+            valence=features.valence,
+        )
+
 
 class SongFeatures(models.Model):
-    id = models.OneToOneField(Song, primary_key=True, on_delete=models.CASCADE)
+    id = models.CharField(primary_key=True, max_length=SPOTIFY_UUID_LENGTH)
     acousticness = models.FloatField()
     danceability = models.FloatField()
     energy = models.FloatField()
@@ -109,3 +131,5 @@ class SongFeatures(models.Model):
     tempo = models.FloatField()
     time_signature = models.FloatField()
     valence = models.FloatField()
+
+    objects = SongFeaturesManager()
