968678e5e90a9a4eae5538d842d1ea3e7a980934
Sergey Sonkin <mastersmail1@gmail.com>
2024-05-17 23:32:58 -0400

    Added test for get_complete_album_from_partial

--- songs/test_spotify_client.py
+++ songs/test_spotify_client.py
@@ -26,7 +26,7 @@ class SpotifyClientTestCase(TestCase):
             client.get_all_artist_albums(artist_id="ARTIST_ID")
             assert get_artist_albums_mock.call_count == 2
 
-    # Todo: Change test to be mock patched
+    # TODO: Change test to be mock patched
     def test_get_album_partials(self):
         client = SpotifyClient()
         albums = client.get_all_artist_albums("7dGJo4pcD2V6oG8kP0tJRR")
@@ -34,3 +34,14 @@ class SpotifyClientTestCase(TestCase):
         self.assertEqual(len(partial_albums), 18)
         for partial_album in partial_albums:
             self.assertEqual(partial_album.next_page, None)
+
+    # TODO: Change test to be mock patched
+    def test_complete_album_partial(self):
+        client = SpotifyClient()
+        albums = client.get_all_artist_albums("2IN2VBkdXOBfoXJGrAUV1O")
+        partial_albums = client.get_album_partials(albums)
+        incomplete_album = partial_albums[1]
+        complete_album = client.get_complete_album_from_partial(incomplete_album)
+
+        self.assertEqual(len(complete_album.tracks), 125)
+        self.assertEqual(len(partial_albums), 8)
