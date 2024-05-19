61aeeaa021017cd1d1a8e79599852f0975c34658
Sergey Sonkin <mastersmail1@gmail.com>
2024-05-18 21:01:23 -0400

    Updated URL handling

--- songs/urls.py
+++ songs/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from songs import views
 
-app_name = "polls"
+app_name = "songs"
 urlpatterns = [
     path("<int:song_id>/", views.detail, name="detail"),
+    path("", views.submit, name="submit"),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+
+def submit(request):
+    return render(request=request, template_name="songs/submit.html")
 
 
 def detail(request, song_id):
-    return HttpResponse(f"You're looking at song_id {song_id}")
+    return HttpResponse("You're looking at song %s." % song_id)
+
+
+@require_POST
+def process_form(request):
+    user_input = request.POST.get("userInput")
+    return redirect("songs/" + user_input)
