eb79b55efdb52f24265bec52145685dd926f5f57
Sergey Sonkin <mastersmail1@gmail.com>
2024-05-19 13:22:37 -0400

    Current state: Reverse for 'process_form' not found

--- songs/urls.py
+++ songs/urls.py
@@ -4,6 +4,7 @@ from songs import views
 
 app_name = "songs"
 urlpatterns = [
+    path("process_form/", views.process_form, name="process_form"),
+    path("submit/", views.submit, name="submit"),
     path("<int:song_id>/", views.detail, name="detail"),
-    path("", views.submit, name="submit"),
 ]
--- songs/views.py
+++ songs/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import redirect, render
 from django.views.decorators.http import require_POST
 
 
+@require_POST
+def process_form(request):
+    user_input = request.POST.get("userInput")
+    return redirect("songs/" + user_input, args=[user_input])
+
+
 def submit(request):
     return render(request=request, template_name="songs/submit.html")
 
 
 def detail(request, song_id):
     return HttpResponse("You're looking at song %s." % song_id)
-
-
-@require_POST
-def process_form(request):
-    user_input = request.POST.get("userInput")
-    return redirect("songs/" + user_input)
