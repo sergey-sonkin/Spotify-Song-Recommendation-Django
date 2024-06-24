9371e6f96d92ce6b4d5e63ae0819a09dd232396f
Sergey Sonkin <mastersmail1@gmail.com>
2024-06-23 22:31:24 -0400

    Fixed a typo. Testing neogit

--- polls/tests.py
+++ polls/tests.py
@@ -46,7 +46,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question_and_past_question(self):
         question = create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future quesiton.", days=30)
+        create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"], [question])
 
